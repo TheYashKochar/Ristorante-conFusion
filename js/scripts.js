f8094519e641e6d2577472155043edbe9e875b77
Yash Kochar <64461707+TheYashKochar@users.noreply.github.com>
2020-10-25 18:38:09 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('#mycarousel').carousel({ interval: 2000 });
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+})
+$(document).ready(function(){
+    $("#restab").click(function(){
+        $('#reservetable').modal('toggle')
+    })
+})
+$(document).ready(function(){
+    $("#login").click(function(){
+        $('#loginModal').modal('toggle')
+    })
+})
